cf0da9de8f3952fa5241fc5d0254759dae6e1e98
Shinjae <pecopong90@gmail.com>
2025-10-20 14:50:42 +0900

    part2 keep going

--- components/Mask.js
+++ components/Mask.js
@@ -24,13 +24,13 @@ export default function Mask() {
             const currentScrollY = window.scrollY;
             const deltaScroll = currentScrollY - lastScrollY;
             lastScrollY = currentScrollY;
-            const delta = 50;
+            const delta = 100;
             const threshold = 0.1;
 
             // 스크롤 다운: 아래쪽만 아주 살짝 줄어듦 (저항감)
             if (deltaScroll > 0) {
                 const targetBottom = bottomMin + delta;
-                currentBottom += (targetBottom - currentBottom) * threshold/5;
+                currentBottom += (targetBottom - currentBottom) * threshold / 5;
 
                 const targetTop = topMax - delta;
                 currentTop += (targetTop - currentTop) * threshold;
@@ -38,10 +38,10 @@ export default function Mask() {
             // 스크롤 업: 위쪽만 아주 살짝 줄어듦 (저항감)
             else if (deltaScroll < 0) {
                 const targetBottom = bottomMin - delta;
-                currentBottom += (targetBottom - currentBottom) * threshold/5;
+                currentBottom += (targetBottom - currentBottom) * threshold;
 
                 const targetTop = topMax + delta;
-                currentTop += (targetTop - currentTop) * threshold;
+                currentTop += (targetTop - currentTop) * threshold / 5;
             }
             // 스크롤 멈춤: 원래대로 복귀
             else {
@@ -52,10 +52,10 @@ export default function Mask() {
             const vw = window.visualViewport?.width || window.innerWidth;
             const vh = window.visualViewport?.height || window.innerHeight;
 
-            const leftPx = (vw ) / 100;
-            const rightPx = (vw ) / 100;
-            const topPx = (vh*3 ) / 100 + currentTop;
-            const bottomPx = (vh*3 ) / 100 + currentBottom;
+            const leftPx = (vw) / 100;
+            const rightPx = (vw) / 100;
+            const topPx = (vh * 14) / 100 + currentTop;
+            const bottomPx = (vh * 14) / 100 + currentBottom;
 
             if (foregroundRef.current) {
                 foregroundRef.current.style.transform = `translateY(${-window.scrollY}px)`;
@@ -247,11 +247,7 @@ export default function Mask() {
                             <div className="d30"></div>
                             <div className="paragraph">소셜미디어 속 저품질 콘텐츠를 과도하게 소비하면 정말로 우리의 지적, 정신적 상태가 저하될까? 그 답을 찾고자 신경과학자, 컴퓨터공학자, 실험심리학자를 만나 ‘내 뇌가 썩고 있다’는 사람들의 호소를 어떻게 분석할 수 있을지 물어봤다.</div>
                             <div className="d30"></div>
-                            <div className="media sweden">
-                                <div className="worldText">
-                                    스웨덴, 스톡홀름<br />카롤린스카 연구소
-                                </div>
-                            </div>
+                            <div className="media sweden world"></div>
                             <div className="d30"></div>
                             <div className="d30"></div>
                             <div className="media peopleSweden"></div>
@@ -272,16 +268,82 @@ export default function Mask() {
                             <div className="d30"></div>
                             <div className="d30"></div>
                             <div className="paragraph">
-                                작업기억과 주의를 담당하는 주요 뇌 영역<br />
-                                1. 배외측 전전두엽 - 이마 바로 뒤쪽, 뇌의 가장 앞부분이다. 정보를 잠시 보관하면서 동시에 조작하는, 작업기억의 핵심 영역이다.
+
+                                <span className="para_subtitle">작업기억과 주의를 담당하는<br />주요 뇌 영역</span>
+                                <br />
+                                <span className="subtitleLine">----</span>
                             </div>
                             <div className="d30"></div>
                             <div className="paragraph">
-                                2. 전측 대상피질 - 대상피질은 뇌의 한가운데를 가로지르는 띠 모양의 피질 구조다. 대상피질의 앞쪽 구역을 전측 대상피질이라 부른다. 중요한 정보에 집중하고 산만한 자극을 억제하는 역할을 한다.
+                                1. 배외측 전전두엽<br />
+                                이마 바로 뒤쪽, 뇌의 가장 앞부분이다. 정보를 잠시 보관하면서 동시에 조작하는, 작업기억의 핵심 영역이다.
                             </div>
                             <div className="d30"></div>
                             <div className="paragraph">
-                                3. 두정엽 - 두정엽은 정수리 쪽에 위치해 있다. 짧은 시간 동안 무언가를 기억할 때 두정엽이 활성화된다. 특히 전두엽-두정엽 간 네트워크가 주의 및 과제 수행 시에 활성화된다.
+                                2. 전측 대상피질<br />
+                                대상피질은 뇌의 한가운데를 가로지르는 띠 모양의 피질 구조다. 대상피질의 앞쪽 구역을 전측 대상피질이라 부른다. 중요한 정보에 집중하고 산만한 자극을 억제하는 역할을 한다.
+                            </div>
+                            <div className="d30"></div>
+                            <div className="paragraph">
+                                3. 두정엽 <br />
+                                두정엽은 정수리 쪽에 위치해 있다. 짧은 시간 동안 무언가를 기억할 때 두정엽이 활성화된다. 특히 전두엽-두정엽 간 네트워크가 주의 및 과제 수행 시에 활성화된다.
+                            </div>
+                            <div className="d30"></div>
+                            <div className="d30"></div>
+                            <div className="d30"></div>
+                            <div className="paragraph">
+                                <span className="para_subtitle">반복되는 자극이 들어올 때<br />뇌 활성</span>
+                                <br />
+                                <span className="subtitleLine">----</span>
+                            </div>
+                            <div className="paragraph quote">“대부분의 사람들이 뇌가 왜, 그리고 어떻게 피로해지는지 모르기 때문에 이를 ‘뇌가 썩었다’고 표현하는 겁니다.”</div>
+                            <div className="d30"></div>
+                            <div className="quoteFrom">-샘슨 니반스 스웨덴 카롤린스카연구소 신경과학부 연구원</div>
+                            <div className="d30"></div>
+                            <div className="d30"></div>
+                            <div className="d30"></div>
+                            <div className="media austrailia world"></div>
+                            <div className="d30"></div>
+                            <div className="media peopleSweden"></div>
+                            <div className="d30"></div>
+                            <div className="d30"></div>
+                            <div className="d30"></div>
+                            <div className="paragraph quote">“반복적으로 하는 모든 일은 뇌를 변화시킵니다. 예를 들어 피아노 연주를 오래 연습하면 섬세한 손가락 움직임을 관장하는 뇌의 영역이 활성화됩니다. 따라서 소셜미디어를 오래 소비하면, 소셜미디어를 사용하기 전과 비교했을 때 뇌가 달라지는 것은 당연한겁니다. 하지만 지금까지 나온 과학적 증거를 살폈을 때, 소셜미디어가 사용자의 자기통제력과 의사결정을 담당하는 전전두엽과 전측 대상피질을 ‘손상’시키는 것은 아닙니다.”</div>
+                            <div className="d30"></div>
+                            <div className="quoteFrom">-오펠 튜렐 호주 멜버른대 컴퓨터공학 및 정보시스템학 교수</div>
+                            <div className="d30"></div>
+                            <div className="d30"></div>
+                            <div className="d30"></div>
+                            <div className="media uk world"></div>
+                            <div className="d30"></div>
+                            <div className="media peopleSweden"></div>
+                            <div className="d30"></div>
+                            <div className="d30"></div>
+                            <div className="d30"></div>
+                            <div className="paragraph quote">“지난 4~5년 동안 기술을 사용할 때 통제력을 잃는다고 말하는 젊은 사람들이 계속해 늘어나고 있는 건 사실입니다. 하지만 소셜미디어가 우리 뇌를 변하게 하는지, 정말 나쁘게 만드는지에 대한 정확한 과학적인 증거는 아직 쌓이는 중입니다. 중요한 건 소셜미디어 사용의 맥락을 살피고, 긴 시간 사용자들의 뇌와 인지 능력을 추적하는 거죠.”</div>
+                            <div className="d30"></div>
+                            <div className="quoteFrom">-에이미 오벤 영국 케임브리지대 MRC 인지 및 뇌과학 연구소 교수</div>
+                            <div className="d30"></div>
+                            <div className="d30"></div>
+                            <div className="d30"></div>
+                            <div className="paragraph">
+                                <span className="para_subtitle">소셜미디어 영향력 연구,<br />어떻게 보완할 것인가 </span>
+                                <br />
+                                <span className="subtitleLine">----</span>
+                            </div>
+                            <div className="media spiral">
+                                <div className="spiralText st1">
+                                    <span>설계분리</span><br />
+                                    메시지만 주고받는 곳인지, 숏폼이 중심인지 등 공간적 특성과 사용자들의 몰입을 구분하고 연구에 반영할 수 있어야 소셜미디어의 영향을 정확하게 측정할 수 있다.
+                                </div>
+                                <div className="spiralText st2">
+                                    <span>장기추적</span><br />
+                                    사람들의 통제력이 약해진다는 점을 증명하기 위해서는 장기간 소셜미디어 사용자들의 통제력을 일관적인 기준으로 추적해야 한다.
+                                </div>
+                                <div className="spiralText st3">
+                                    <span>맥락을 복원</span><br />
+                                    소셜미디어 속 콘텐츠는 다양하고 복합적이며, 무엇보다 해당 콘텐츠를 소비하는 상황과 맥락이 함께 존재한다. 유튜브를 60분가량 시청하면서도 어떤 영상을 어떤 목적에 의해 보는지에 따라 사용자의 경험과 소셜미디어가 미치는 영향은 다르다. 소셜미디어의 실제 효과와 영향력을 제대로 측정하려면 이런 맥락을 살펴야 한다.
+                                </div>
                             </div>
                         </div>
                     </div>
