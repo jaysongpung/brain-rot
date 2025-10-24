faa3f2f482244609d64944f5d5392d7718de183b
Shinjae <pecopong90@gmail.com>
2025-10-24 14:05:03 +0900

    part3 ongoing

--- components/part3.js
+++ components/part3.js
@@ -1,6 +1,88 @@
+'use client';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
 export default function Part3Content() {
     return (
         <>
+            <div className="partTitle">
+                브레인롯 탈출할 수 있을까? <br />
+                <span>숏폼 4주 끊기</span> 챌린지
+            </div>
+            <div className="d30"></div>
+            <div className="d30"></div>
+            <div className="d30"></div>
+            <div className="chatContainer">
+                <div className="chat chat-left">시간을 낭비하는 느낌이에요.</div><br />
+                <div className="chat chat-right">자기 전 습관처럼 숏폼을 보다가 늦게 잠드는 경우가 많아요.</div><br />
+                <div className="chat chat-left">장시간 숏폼을 보면, 정신이 멍해지는 느낌이 들어요.</div><br />
+                <div className="chat chat-right">긴 영상은 답답해서 보지 않게 되더라고요.</div><br />
+                <div className="chat chat-left">스마트폰을 손에서 못 놓고 계속 잡고 있어요.</div><br />
+                <div className="chat chat-right">힘들었던 날은 유독 더 오래 숏폼을 봐요. 그런데 다 보고 나면 그냥 그 시간에 자는 게 나았다는 생각이 들더라고요.</div><br />
+                <div className="chat chat-left">집중력이 낮아진 느낌이에요.</div><br />
+                <div className="chat chat-right">누워서 숏폼만 봤을 뿐인데, 늘 피곤하고 무기력한 사람이 돼있었어요.</div>
+            </div>
+            <div className="d30"></div>
+            <div className="d30"></div>
+            <div className="d30"></div>
+            <div className="paragraph">
+                우리는 왜 피곤해졋을까. 과연 브레인롯에서 탈출할 수 있을까. 과학동아가 진행한 &apos;숏폼 4주 끊기&apos; 챌린지 참가자들이 출발선에서 한 말입니다. 브레인롯은 아직 의학적 진단명은 아니지만, 학자들은 &apos;소셜미디어를 반복해 접한 결과, 뇌에 부하가 가해지면서 생긴 인지적 피로&apos;라고 해석합니다.
+            </div>
+            <div className="d30"></div>
+
+            <div className="paragraph">
+                피로해졌다면, 쉬면 됩니다. 뇌도 마찬가지일까요?
+            </div>
+            <div className="d30"></div>
+            <div className="d30"></div>
+            <div className="d30"></div>
+            <div className="paragraph">
+                이 질문에 대한 답을 찾기 위해 과학동아는 8월 26일부터 9월 22일까지 약 <span className="bold">4주</span>간 29명의 참가자들과 &apos;숏폼 4주 끊기&apos; 챌린지를 시작했습니다.
+                참가자들이 끊은 소셜미디어 서비스는 <span className="bold">인스타그램 릴스, 유튜브 숏츠, X, 틱톡</span>이었습니다.
+                모두 추천 알고리즘이 숏폼을 무한히 보도록 만드는 매체입니다.
+            </div>
+            <div className="d30"></div>
+            <div className="paragraph">
+                <span class="bold">29명</span>의 참가자 중 중도이탈자는 단 3명이었습니다.<br />
+                <span class="bold">26명</span>의 이야기를 과학적으로 분석해봤습니다.
+            </div>
+            <div className="d30"></div>
+            <div className="littlequote">*김주현 한국뇌연구원 선임연구원과 조철현 고려대 정신건강의학과 교수가 챌린지 설계와 결과 분석 등의 자문을 맡았습니다.</div>
+            <div className="d30"></div>
+            <div className="d30"></div>
+            <div className="d30"></div>
+            <div className="paragraph">
+                챌린지 참가자들은 모두 <span className="bold">1주일에 13시간 이상 숏폼 콘텐츠를 소비</span>하며 <span className="bold">자신이 숏폼으로 인한 브레인롯을 겪고 있다고 느끼는 이들</span>이었습니다.
+            </div>
+            <div className="d30"></div>
+            <div className="paragraph">
+                마지막까지 함께한 26인은 챌린지에 참여하기 직전 1주일과 챌린지 참여 마지막 주인 4주차를 비교했을 때, 주간 숏폼 이용 시간을
+            </div>
+            <div className="d30"></div>
+            <div className="hourglass-container">
+                <div className="hourglass-text">많게는 58시간 32분</div>
+                <div className="lottie_hourglass">
+                    <DotLottieReact
+                        src="/hourglass.json"
+                        loop
+                        autoplay
+                    />
+                </div>
+                <div className="hourglass-text">적게는 3시간 17분까지 줄였습니다</div>
+            </div>
+            <div className="d30"></div>
+            <div className="d30"></div>
+            <div className="d30"></div>
+            <div className="paragraph">
+                <span className="para_subtitle">뇌파</span>
+                <br />
+                <span className="subtitleLine">----</span>
+                <br />
+                챌린지 전과 후, 가장 궁금했던 건 뇌의 변화였습니다.
+                챌린지 전, 브레인롯 증상을 보이며 숏폼 앱 이용시간이 긴 참가자들의 뇌는 어떻게 다른지 궁금했습니다.
+                챌린지 후, 이들이 4주간 숏폼을 끊고 나면, 뇌가 실제로 회복할지도 알고 싶었습니다.
+                그래서 뇌파 측정 전문 기업 &apos;아이메디신&apos;의 비침습형 뇌파 측정 장치 &apos;아이싱크웨이브&apos;를 이용해 뇌파를 측정했습니다. 아이싱크웨이브는 19개의 전극으로 뇌의 각 부위에서 관찰되는 뇌파를 주파수 대역별로 측정합니다.
+
+            </div>
         </>
 
     )
